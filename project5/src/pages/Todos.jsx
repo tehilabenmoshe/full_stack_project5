014d46207fe4244af0577f42940ca7d3dd073828
תהילה בן משה <tbenmosh@g.jct.ac.il>
2025-05-18 13:08:39 +0300

    add todo

--- project5/src/pages/Todos.jsx
+++ project5/src/pages/Todos.jsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { getTodos } from "../services/todosService";
+import TodoItem from "../components/TodoItem"; 
 
 export default function Todos() {
-    return <h1>Todos</h1>;
+  const [todos, setTodos] = useState([]);
+
+  const user = JSON.parse(localStorage.getItem("user"));
+  const userId = user.id;
+
+
+  useEffect(() => {
+    if (userId) {
+      getTodos(userId).then(setTodos).catch((err) => {
+        console.error("Failed to load todos:", err);
+      });
+    }
+  }, [userId]);
+
+  return (
+    <div>
+      <h2>My Todos</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input type="checkbox" checked={todo.completed} readOnly />
+            <span style={{ marginLeft: "8px" }}>
+              #{todo.id} - {todo.title}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
