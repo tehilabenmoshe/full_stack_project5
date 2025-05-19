d4b04040afceb7b6560260a6e54252f8fe70418e
TamarHayat <tahayat@g.jct.ac.il>
2025-05-19 14:44:19 +0300

    add basic home page

--- project5/src/pages/Home.jsx
+++ project5/src/pages/Home.jsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
-    return <h1>Home</h1>;
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem('user');
+    if (savedUser) setUser(JSON.parse(savedUser));
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome{user ? `, ${user.name || user.username}` : ''}!</h1>
+      <div style={{ margin: 30 }}>
+        <button onClick={() => navigate('/todos')} >Todos</button>
+        <button onClick={() => navigate('/posts')} >Posts</button>
+        <button onClick={() => navigate('/albums')} >Albums</button>
+        <button onClick={() => navigate('/info')} >Info</button>
+      </div>
+    </div>
+  );
 }
