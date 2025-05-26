7a41b4aa96ba1a257ff93d770e8417b75a4d6c21
תהילה בן משה <tbenmosh@g.jct.ac.il>
2025-05-26 11:23:33 +0300

    finish the home page

--- project5/src/pages/Home.jsx
+++ project5/src/pages/Home.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import mainImg from '../assets/main.png';
+import todosImg from '../assets/todos.png';
+import postsImg from '../assets/posts.png';
+import albumsImg from '../assets/albums.png';
+import albumsImg2 from '../assets/albums2.png';
+
 
 export default function Home() {
   const [user, setUser] = useState(null);
@@ -14,8 +20,8 @@ export default function Home() {
     <div className="home-container">
       <section className="hero-section">
         <div className="hero-text">
-          <h1>Welcome Back{user ? `, ${user.username}` : ''}!</h1>
-          <p>Your gateway to managing tasks, posts, and more!</p>
+          <h1>Welcome Back, {user ? ` ${user.username}` : ''}!</h1>
+          <p>Your gateway to managing tasks, posts, and more! <br/> So let's get start!!</p>
           <div className="hero-buttons">
             <button onClick={() => navigate('/todos')}>Todos</button>
             <button onClick={() => navigate('/posts')}>Posts</button>
@@ -24,7 +30,7 @@ export default function Home() {
           </div>
         </div>
         <div className="hero-image">
-          <img src="./assets/albums.png" alt="dashboard illustration" />
+          <img className="albums-img" src={mainImg} alt="dashboard illustration" />
         </div>
       </section>
 
@@ -34,6 +40,9 @@ export default function Home() {
           <p>Track your Todos, manage Posts, view Albums and Info with a modern and user-friendly interface.</p>
           <button className="demo-button" onClick={() => navigate('/todos')}>Todos</button>
         </div>
+          <div className="feature-image">
+            <img src={todosImg} alt="Todos Illustration" />
+          </div>   
       </section>
 
       <section className="hero-section">
@@ -42,6 +51,9 @@ export default function Home() {
           <p>Track your Todos, manage Posts, view Albums and Info with a modern and user-friendly interface.</p>
           <button className="demo-button" onClick={() => navigate('/posts')}>Posts</button>
         </div>
+         <div className="feature-image">
+            <img src={postsImg} alt="Todos Illustration" />
+          </div>
       </section>
 
       <section className="feature-section">
@@ -49,6 +61,9 @@ export default function Home() {
           <h2>WATCH YOUR ALBUMS</h2>
           <p>Track your Todos, manage Posts, view Albums and Info with a modern and user-friendly interface.</p>
           <button className="demo-button" onClick={() => navigate('/albums')}>Albums</button>
+        </div>
+        <div className="feature-image">
+          <img src={albumsImg} alt="Todos Illustration" />
         </div>
       </section>
 
